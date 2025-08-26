de30fefeeafb89c9a36e73b30c463ffc90a72570
Deivi Robles <drozam24@gmail.com>
2025-08-25 23:11:39 -0400

    Middleware implementation

--- app/index.js
+++ app/index.js
@@ -3,5 +3,22 @@ import express from "express";
 export const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    message: "Welcome to Checklist API"
+  });
+});
+
+//No route found handler
+app.use((req, res, next) => {
+  next({status: 404, message: "Error, Route not found"});
+});
+
+//error handler
+app.use((err, req, res, next) => {
+  const {status = 500, message} = err;
+
+  res.status(status);
+  res.json({
+    error : { status, message }
+  });
 });
